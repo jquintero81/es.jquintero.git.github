fa83a51a18a9a7a466099d5d6c0df7294a13d281
Jesús <jquinh@hotmail.com>
2015-04-11 09:31:48 +0200

    Primer cambio en la rama develop

--- ejemploMaven1/src/main/java/es/maven/ejemplo/ejemploMaven1/App.java
+++ ejemploMaven1/src/main/java/es/maven/ejemplo/ejemploMaven1/App.java
@@ -5,7 +5,7 @@ package es.maven.ejemplo.ejemploMaven1;
  *
  */
 public class App 
-{   //
+{   // 
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
