704d68fcd7805218feceec2283cfcab36263a460
Jesús <jquinh@hotmail.com>
2015-04-11 12:49:29 +0200

    [release-1.0] Solucionado el bug 1

--- ejemploMaven1/src/main/java/es/maven/ejemplo/ejemploMaven1/App.java
+++ ejemploMaven1/src/main/java/es/maven/ejemplo/ejemploMaven1/App.java
@@ -8,6 +8,7 @@ public class App
 {   // 
     public static void main( String[] args )
     {
+    	//Solucionamos el bug 1 de la v1.0 en release
         System.out.println( "Hello World!" );
     }
 }
